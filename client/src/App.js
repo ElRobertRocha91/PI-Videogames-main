9faf80320e2b304c19118990a15014582d2f7e7b
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-02-28 02:17:52 -0300

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+//Me importo mi:
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import LandingPage from './components/landingPage/LandingPage';
+import Home from './components/home/Home';
+import Detail from './components/detail/Detail';
+import Form from './components/form/Form';
+import Error404 from './components/error404/Error404';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <h1>Henry Videogames</h1>
+      <Switch>
+        <Route exact path='/' component={LandingPage}/>
+        <Route path='/home' component={Home}/>
+        <Route path='/detail/:id' component={Detail}/>
+        <Route path='/createVideogames' component={Form}/>
+        <Route path='*' component={Error404}/>
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 

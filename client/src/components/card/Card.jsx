76725150e7cb46e0a9b5476ae3993482196d70ff
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-03-13 15:22:41 -0300

    last changes

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -4,12 +4,14 @@ import style from "./Card.module.css";
 //Recibimos Props como argumentos
 export default function Card({id, name, genres, rating, image}) {
     return(
-        <div>
+        <div className={style.container}>
           <div className={style.card}>
             <img src={image} alt={name} className={style.img}/>
-            <h1>{name}</h1>
-            <h3>Generos: {genres}</h3>
-            <h3>Rating: {rating}</h3>
+            <div className={style.text}>
+              <h3>{name}</h3>
+              <h6>Generos: {genres}</h6>
+              <h6>Rating: {rating}</h6>
+            </div>
           </div>
         </div>
     )

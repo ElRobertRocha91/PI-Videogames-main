00beacc5bc52f93ca9aca1ca7dfac51dd9e1348b
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-08-11 23:53:46 -0300

    handler: { getGamesHandler-updated }

--- api/src/handlers/videoGameHandler.js
+++ api/src/handlers/videoGameHandler.js
@@ -1,51 +1,24 @@
 const { getAllGames } = require('../controllers/videoGameController');
-const axios = require('axios');
-const { MY_API_KEY } = process.env;
-
-
-//Funciones manejadoras para VIDEOGAMES
+const { getGamesByName } = require('../controllers/videoGameNameController');
 
 const getGamesHandler = async (req, res) => {
-    //console.log('RUTA A http://localhost:3001/videogames?&key=... Y A http://localhost:3001/videogames?name="..."&key=...');
+    // console.log('RUTA A http://localhost:3001/videogames?&key=... Y A http://localhost:3001/videogames?name="..."&key=...');
     try {
-        //Si el cliente introduce un name por query
         const { name } = req.query;
-        //Valido si existe name:
+        // Validate name
         if(name){
-            //voy a buscarlo a la API
-            const dataApi = await axios(`https://api.rawg.io/api/games?search=${name}&key=${MY_API_KEY}`);
-            //me trae la info .data
-            //console.log(dataApi)
-            //valido si dataApi existe:
-            if(!dataApi.data.count){
-                res.status(404).json({msg: "Videogame not found"});
-            } 
-            //Valido que el array results sea mayor a 15, si lo es, solo traeme 15 elementos del array
-            if(dataApi.data.results.length > 15){
-                dataApi.data.results.length = 15
+            const videogames = await getGamesByName(name);
+            
+            if(videogames.length){
+                res.status(200).json(videogames)
             }
-            //hasta aqui ya tengo mis 15 videogames, ahora mapeamos esa data y retornamos un array con esos 15 obj
-            const gameApi = dataApi.data.results.map(el => {
-                return {
-                    id: el.id,
-                    name: el.name,
-                    description: el.description,
-                    released: el.released,
-                    rating: el.rating,
-                    image: el.background_image,
-                    genres: el.genres.map(genre => genre.name).join(' '),
-                    platform: el.platforms.map((el) => el.platform.name).join(' ')
-                }
-            })
-            //Respondemos con el array de objetos que creamos
-            res.status(200).json(gameApi);
+            throw new Error({message: "Videogame not found"}); 
         }else{
-            //Si no se recibe un name por query, le devolvemos todos los videogames
             const allGames = await getAllGames();
             res.status(200).json(allGames);
         }
     } catch (error) {
-        console.log(error);
+        res.status(404).json(error.message);
     }
 }
 
